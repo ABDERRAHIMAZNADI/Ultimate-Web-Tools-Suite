178c354291bbf41849c42c4e4286309843e5c7b7
ABDERRAHIMAZNADI <zegezzaffe@gmail.com>
2026-06-07 12:00:36 +0100

    Add files via upload

---
+++ Next.js
@@ -0,0 +1,13 @@
+import type { AppProps } from 'nextapp';
+import { SpeedInsights } from '@vercelspeed-insightsnext';
+ 
+function MyApp({ Component, pageProps } AppProps) {
+  return (
+    
+      Component {...pageProps} 
+      SpeedInsights 
+    
+  );
+}
+ 
+export default MyApp;
